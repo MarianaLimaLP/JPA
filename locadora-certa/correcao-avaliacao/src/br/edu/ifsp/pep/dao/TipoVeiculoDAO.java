e310be71ed9d0f5456899913da771c949f8ec91f
Mariana Lima Liberto Pedreira <mariana_e_fer@hotmail.com>
2022-09-30 15:38:44 -0300

    Visao

--- locadora-certa/correcao-avaliacao/src/br/edu/ifsp/pep/dao/TipoVeiculoDAO.java
+++ locadora-certa/correcao-avaliacao/src/br/edu/ifsp/pep/dao/TipoVeiculoDAO.java
@@ -1,11 +1,16 @@
 package br.edu.ifsp.pep.dao;
 
 import br.edu.ifsp.pep.modelo.TipoVeiculo;
+import java.util.List;
 
-/**
- *
- * @author aluno
- */
 public class TipoVeiculoDAO extends AbstractDAO<TipoVeiculo>{
+    
+    public List<TipoVeiculo> buscarTodos() {
+        return getEntityManager().createNamedQuery("TipoVeiculo.buscarTodos", TipoVeiculo.class).getResultList();
+    }
+    
+    public List<TipoVeiculo> findByNome(String nome) {
+        return getEntityManager().createNamedQuery("TipoVeiculo.findByNome", TipoVeiculo.class).setParameter("nome", "%"+nome+"%").getResultList();
+    }
     
 }
